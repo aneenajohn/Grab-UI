ed56651cb1dd5a6943a9e2a30069064b17d03dba
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-03-25 00:37:13 +0530

    Added components

---
+++ Components/Modal/modal.js
@@ -0,0 +1,17 @@
+var btn = document.getElementById("openBtn");
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
